aa5e81b5d1aa8ac980b1a605ed8ae6b192dc4833
kzusg <94691499+KAZUKI-0130@users.noreply.github.com>
2022-12-03 08:52:38 +0900

    Update bot_join.js

--- events/bot_join.js
+++ events/bot_join.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 const config = require("../config.js");
 const functions = require("../functions.js");
+const invite = await
+
+//リンク抜き取り
+
 
 //サーバー参加
 module.exports = {
@@ -8,8 +12,9 @@ module.exports = {
   async execute(guild, client) {
     client.user.setActivity({name: `${client.guilds.cache.size}servers`,type: "WATCHING"});
   const addEmbed = new Discord.MessageEmbed()
-    .setTitle("サーバー追加")
-    .setDescription(`${guild.name}(${guild.id})にBotが追加されました。`)
+    .setTitle("Bot_Join")
+    .setDescription(`NAME : ${guild.name}`　+ "\n" + `ID : ${guild.id}`)
+    .addField("") //リンク出力
     .setThumbnail(guild.iconURL({ dynamic: true }))
     .setColor(config.color)
     .setTimestamp();
