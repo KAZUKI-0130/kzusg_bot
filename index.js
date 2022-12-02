266474be57d8f51cf93f11a6725d5d9f8bf852c8
kzusg <94691499+KAZUKI-0130@users.noreply.github.com>
2022-12-03 05:22:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,10 @@ try {
     .setTimestamp()
     .setThumbnail(i.user.displayAvatarURL({ dynamic: true }))
     .addField("コマンド", "```\n" + `${command.data.name}` + "\n```")
-    .addField("実行サーバー", "```\n" + `${i.guild.name}` + `(${i.guild?.id ?? "DM"})` + "\n```", true)
-    .addField("実行ユーザー", "```\n" + `${i.user.tag}` + `(${i.user.id})` + "\n```", true)
+    .addField("実行サーバー", "```\n" + `${i.guild.name}` 
+              `(${i.guild?.id ?? "DM"})` + "\n```", true)
+    .addField("実行ユーザー", "```\n" + `${i.user.tag}`
+              `(${i.user.id})` + "\n```", true)
     .setFooter("")
   client.channels.fetch(config.logch.command).then(c => c.send({ embeds: [log] }));
 } catch (error) {
